1015bdf702a3630d7b1578a83fb692919007bc8c
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2020-12-01 16:18:49 -0300

    Initial Commit

---
+++ Animaciones/AnimacionesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimacionesApp.swift
+//  Animaciones
+//
+//  Created by Jacob Aguilar on 01-12-20.
+//
+
+import SwiftUI
+
+@main
+struct AnimacionesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
